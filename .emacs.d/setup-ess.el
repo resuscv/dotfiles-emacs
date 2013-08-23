9431097ac64f6c9ed77bd7ac26c736c49c350ab2
Olaf TNSB <still.another.person@gmail.com>
2013-08-23 15:18:07 +1000

    Added comint commands, from the manual

--- .emacs.d/setup-ess.el
+++ .emacs.d/setup-ess.el
@@ -5,6 +5,18 @@
   (setq ess-ask-for-ess-directory nil)
   (require 'ess-site nil 'noerror))
 
+(eval-after-load "comint"
+  '(progn
+     (define-key comint-mode-map [up]
+       'comint-previous-matching-input-from-input)
+     (define-key comint-mode-map [down]
+       'comint-next-matching-input-from-input)
+     ;; also recommended for ESS use --
+     (setq comint-scroll-to-bottom-on-output 'others)
+     (setq comint-scroll-show-maximum-output t)
+     ;; somewhat extreme, almost disabling writing in *R*, *shell* buffers above prompt:
+     (setq comint-scroll-to-bottom-on-input 'this)
+     ))
 
 ;; Almost END
 (provide 'setup-ess)
