273ad887aecf8bbda945445993416a3ba554b59e
Olaf TNSB <still.another.person@gmail.com>
2013-09-30 09:32:22 +1000

    Start/Stop emacs server

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -10,6 +10,18 @@
 ;; Added a new line at the end of files
 (setq require-final-newline t)
 
+;; define function to shutdown emacs server instance
+(defun server-shutdown ()
+  "Save buffers, Quit, and Shutdown (kill) server"
+  (interactive)
+  (save-some-buffers)
+  (kill-emacs)
+  )
+
+;; As emacsclient doesn't get the terminal background colour, it doesn't correctly set org-face
+;; so org-startup-indented doesn't work properly.  So go old school and just start the server.
+(server-start)
+
 ;;----------------------------------------------------------------------------
 ;; Load configs for specific features and modes
 ;;----------------------------------------------------------------------------
